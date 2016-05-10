c17f024680238d441350ed47f84e81dd70c3ef17
Caleb Brown <killerdillercaleb@gmail.com>
2016-05-10 11:30:05 -0400

    Updated the packages for react 15.0.2

--- src/routes.js
+++ src/routes.js
@@ -13,15 +13,12 @@ import {
 export default function routes() {
   return (
     <Route component={App} path="/">
-      { /* Home (main) route */ }
       <IndexRoute component={Place} />
       <Route component={About} path="about" />
       <Route component={Contact} path="contact" />
       <Route component={Place} path="places/:slug">
         <Route component={Place} path=":subdir" />
       </Route>
-
-      { /* Catch all route */ }
       <Route component={NotFound} path="*" status={404} />
     </Route>
   );
