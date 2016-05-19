2d0bf716f397a422527ad4700176d3c6e342c0b3
Caleb Brown <killerdillercaleb@gmail.com>
2016-05-19 08:52:08 -0500

    Added media and updated data for springfield

--- src/redux/modules/map.js
+++ src/redux/modules/map.js
@@ -163,10 +163,24 @@ const initialState = {
           day: 4,
           fullTitle: 'Springfield, IL',
           id: 5,
-          miles: 461,
+          media: [
+            '<blockquote class="instagram-media" data-instgrm-captioned data-instgrm-version="7" style=" background:#FFF; border:0; border-radius:3px; box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15); margin: 1px; max-width:658px; padding:0; width:99.375%; width:-webkit-calc(100% - 2px); width:calc(100% - 2px);"><div style="padding:8px;"> <div style=" background:#F8F8F8; line-height:0; margin-top:40px; padding:37.5% 0; text-align:center; width:100%;"> <div style=" background:url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACwAAAAsCAMAAAApWqozAAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAAAMUExURczMzPf399fX1+bm5mzY9AMAAADiSURBVDjLvZXbEsMgCES5/P8/t9FuRVCRmU73JWlzosgSIIZURCjo/ad+EQJJB4Hv8BFt+IDpQoCx1wjOSBFhh2XssxEIYn3ulI/6MNReE07UIWJEv8UEOWDS88LY97kqyTliJKKtuYBbruAyVh5wOHiXmpi5we58Ek028czwyuQdLKPG1Bkb4NnM+VeAnfHqn1k4+GPT6uGQcvu2h2OVuIf/gWUFyy8OWEpdyZSa3aVCqpVoVvzZZ2VTnn2wU8qzVjDDetO90GSy9mVLqtgYSy231MxrY6I2gGqjrTY0L8fxCxfCBbhWrsYYAAAAAElFTkSuQmCC); display:block; height:44px; margin:0 auto -44px; position:relative; top:-22px; width:44px;"></div></div> <p style=" margin:8px 0 0 0; padding:0 4px;"> <a href="https://www.instagram.com/p/BFkJqtTSphR/" style=" color:#000; font-family:Arial,sans-serif; font-size:14px; font-style:normal; font-weight:normal; line-height:17px; text-decoration:none; word-wrap:break-word;" target="_blank">Somewhere, IL. On our way to Springfield. #tbmaga</a></p> <p style=" color:#c9c8cd; font-family:Arial,sans-serif; font-size:14px; line-height:17px; margin-bottom:0; margin-top:8px; overflow:hidden; padding:8px 0 7px; text-align:center; text-overflow:ellipsis; white-space:nowrap;">A photo posted by Lillian (@lillian_brown) on <time style=" font-family:Arial,sans-serif; font-size:14px; line-height:17px;" datetime="2016-05-18T22:10:21+00:00">May 18, 2016 at 3:10pm PDT</time></p></div></blockquote>'
+          ],
+          miles: 506,
           next: 6,
+          photos: 15,
           prev: 4,
           slug: 'springfield',
+          soundtrack: [
+            {
+              title: 'Roger Miller - King of the Road',
+              img: 'roger-miller.jpg'
+            },
+            {
+              title: 'Bob Dylan - Highway 61',
+              img: 'bob.jpg'
+            }
+          ],
           title: 'Springfield',
           why: 'Caleb\'s dad lives here.'
         },
